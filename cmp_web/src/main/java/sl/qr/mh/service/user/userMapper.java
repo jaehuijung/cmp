4dad0957dfb24529df545452802bd01ee3b0e673
mh <sl>
2024-10-14 21:30:19 +0900

    사용자관리 > 사용자목록 >  추가상세수정
    뭐여 이거 왜 빠졌냐

--- cmp_web/src/main/java/sl/qr/mh/service/user/userMapper.java
+++ cmp_web/src/main/java/sl/qr/mh/service/user/userMapper.java
@@ -8,15 +8,16 @@ import java.util.Map;
 @Mapper
 public interface userMapper {
     // 사용자 정보 리스트
-    public List<Map<String, Object>> getAllUserList(Map<String, Object> paramMap);
+    public List<Map<String, Object>> getUserList(Map<String, Object> paramMap);
     
     // 사용자 정보 리스트 개수
-    public int getAllUserListCnt(Map<String, Object> paramMap);
-    
+    public int getUserListCnt(Map<String, Object> paramMap);
+
+    // 선택된 사용자 정보 리스트
+    public Map<String, Object> getSelectUserList(Map<String, Object> paramMap);
+
     // 사용자 정보 저장
     public void saveUserInfo(Map<String, Object> paramMap);
-
-
 
     // 사용자 부서 리스트
     public List<Map<String, Object>> getUserDepartmentList();
