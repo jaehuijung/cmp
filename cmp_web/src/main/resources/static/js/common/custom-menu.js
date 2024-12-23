2f2bf55f52424cc5c1ce9446146987656a7caeb3
구명회 <sl>
2024-12-23 15:53:56 +0900

    공통 레이아웃 구성 1차 완료

--- cmp_web/src/main/resources/static/js/common/custom-menu.js
+++ cmp_web/src/main/resources/static/js/common/custom-menu.js
@@ -31,27 +31,78 @@ function buildMenus(menuData) {
 }
 
 // 왼쪽 사이드메뉴 구성
-function buildSideMenu2(menuData, currentUrl) {
+function buildSideMenu(menuData, currentUrl) {
     const currentMenu = getParentMenuId(menuData, currentUrl);
-
     if (!currentMenu) {
         alert2("알림", "메뉴를 찾을 수 없는 오류가 발생했습니다. <br> 새로고침 후 문제가 지속되면 관리자에게 문의하세요.", "info", "확인");
         return;
     }
 
+    // 필터링: 최상위 메뉴 (parent_menu_id가 00000000)
     const sideMenuContainer = $('#menu-items');
-    const filteredMenu = menuData.filter(menu => menu.parent_menu_id === '00000000');
-
-    const sideMenuHtml = filteredMenu
-        .map(menu => {
+    const topLevelMenus = menuData.filter(menu => menu.parent_menu_id === '00000000');
+    const sideMenuHtml =
+        topLevelMenus.map(menu => {
             const isActive = menu.menu_id === currentMenu.menu_id ? ' active' : '';
-            return `<a href="${menu.url}" class="side-menu-item${isActive}"><img src="${menu.icon}" alt="icon" class="menu-icon" />${menu.menu_name}</a>`;
-        })
-        .join('');
+            const subMenus = menuData.filter(subMenu => subMenu.parent_menu_id === menu.menu_id);
+            const subMenuHtml = subMenus.length > 0
+                ? `
+                    <ul class="dropdown-menu" data-bs-display="static" data-menu-id="${menu.menu_id}">
+                        ${subMenus
+                            .map(subMenu => {
+                                const subActive = subMenu.menu_id === currentMenu.menu_id ? ' active' : '';
+                                return `
+                                <li data-menu-id="${subMenu.menu_id}">
+                                    <a href="${subMenu.url}" class="dropdown-item${subActive}">
+                                        <img src="${subMenu.icon}" alt="icon" class="menu-icon" />
+                                        ${subMenu.menu_name}
+                                    </a>
+                                </li>
+                                `;
+                            })
+                            .join('')}
+                    </ul>
+                `: '';
 
+            // 부모 메뉴 및 하위 메뉴 HTML 생성
+            return `
+                <div class="menu-item dropdown" data-menu-id="${menu.menu_id}">
+                    <a href="${subMenus.length > 0 ? '#' : menu.url}"
+                       class="side-menu-item dropdown-toggle ${isActive}"
+                       ${subMenus.length > 0 ? 'data-bs-toggle="dropdown" aria-expanded="false"' : ''}>
+                        <img src="${menu.icon}" alt="icon" class="menu-icon" />
+                        ${menu.menu_name}
+                    </a>
+                    ${subMenuHtml}
+                </div>`;
+        }).join('');
 
     if (sideMenuContainer.length > 0) {
         sideMenuContainer.html(sideMenuHtml);
+    }
+
+    openActiveDropdowns(currentMenu.menu_id, menuData); // 페이지 렌더링 후 활성화된 메뉴의 부모 드롭다운 열기
+}
+
+function openActiveDropdowns(menuId, menuData) {
+    let parentId = menuId;
+
+    while (parentId && parentId !== '00000000') {
+        const parentMenu = menuData.find(menu => menu.menu_id === parentId);
+
+        if (parentMenu) {
+            const parentElement = $(`[data-menu-id="${parentMenu.menu_id}"]`);
+            const dropdownMenu = parentElement.closest('.menu-item').find('.dropdown-menu');
+            const dropdownToggle = parentElement.closest('.side-menu-item').find('.dropdown-toggle');
+
+            dropdownMenu.addClass('show');
+            parentElement.addClass('active');
+            dropdownToggle.attr('aria-expanded', 'true');
+
+            parentId = parentMenu.parent_menu_id;
+        } else {
+            break;
+        }
     }
 }
 
